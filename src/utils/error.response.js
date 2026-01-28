70099dd47c31f017de20b5000cfd1ea428d0579d
linhnguyen02 <nguyenduykhanhlinh@javis.vn>
2026-01-29 01:02:13 +0700

    update mess khi bài kiểm tra không có câu hỏi

--- src/utils/error.response.js
+++ src/utils/error.response.js
@@ -1,11 +1,10 @@
 export const hanldeError = (code, msgAuto) => {
   if (!code) return msgAuto ? msgAuto : "Lỗi không xác định";
-  console.log("Handling error code:", code);
   if (code === "no_more_turns") return "Bạn đã hết lượt làm bài";
   else if (code === "exam_not_found") return "Không tìm thấy đề thi";
-  else if (code === "overdue_doing_exam") {
-    return "Đã qua thời gian làm bài";
-  }
+  else if (code === "overdue_doing_exam") return "Đã qua thời gian làm bài";
+  else if (code == "no_question_in_exam")
+    return "Đề thi không hợp lệ do không có câu hỏi";
 
   return "Hệ thống đang gặp sự cố, vui lòng thử lại sau";
 };
